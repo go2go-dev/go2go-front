9506a0be967499fa4604d0fa4259f28c528c982c
GuNayeon77 <listoss456@gmail.com>
2025-07-13 01:36:43 +0900

    todo 페이지 배경 색상 동적 처리

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 interface MobileLayoutProps {
   children: React.ReactNode;
 }
 
 export default function MobileLayout({ children }: MobileLayoutProps) {
+  const location = useLocation();
+  const isTodoPage = location.pathname === '/todo'; // 경로 비교
+
   return (
-    <div className="bg-white flex flex-row justify-center w-full min-h-screen">
-      <div className="bg-white w-[390px] h-[844px] relative px-5">{children}</div>
+    <div className={`flex flex-row justify-center w-full min-h-screen ${isTodoPage ? 'bg-100' : 'bg-white'}`}>
+      <div className={`w-[390px] h-[844px] relative px-5 ${isTodoPage ? 'bg-100' : 'bg-white'}`}>
+        {children}
+      </div>
     </div>
   );
 }
